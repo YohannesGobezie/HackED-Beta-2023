46a02fceaeea970ef994cd642bc5a95261bf575c
Yohannes <yohannes.gobezie@gmail.com>
2023-11-18 16:17:20 -0700

    Changing texts based on navbar

--- shush_app/lib/main.dart
+++ shush_app/lib/main.dart
@@ -5,6 +5,13 @@ void main() {
   runApp(const MyApp());
 }
 
+const TextStyle _textStyle = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 2,
+  fontStyle: FontStyle.italic,
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -58,6 +65,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
 
+  List<Widget> pages = const [
+    Text('Home', style: _textStyle),
+    Text('Add', style: _textStyle),
+    Text('Profile', style: _textStyle),
+  ];
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -68,13 +81,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      body: Center(
+        child: pages[_currentIndex],
       ),
       bottomNavigationBar: NavigationBar (
         backgroundColor: Color.fromARGB(131, 239, 203, 44),
@@ -102,11 +113,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      // body: Center(
-      //   // Center is a layout widget. It takes a single child and positions it
-      //   // in the middle of the parent.
-
-      // ),
     );
   }
 }
